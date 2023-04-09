d7a7197fbbf920eb1f4a58e98ac5131cbabe8ca0
Gustavo Fernandes <gfernandesilva2904@gmail.com>
2023-04-09 20:16:51 -0300

    correção do bug da migration clothing

--- database/migrations/2023_03_30_225142_create_clothing_table.php
+++ database/migrations/2023_03_30_225142_create_clothing_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->float('price',8,2);
             $table->string('patch', 45);
             $table->foreignId('category_id');
-            $table->foreignId('user_id');
+            $table->varchar('user_id',25);
             $table->foreignId('collection_id');
             $table->timestamps();
         });
