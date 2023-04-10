f58044219120495876e35eabfcb2e6c629d7464d
Gustavo Fernandes <gfernandesilva2904@gmail.com>
2023-04-09 22:07:45 -0300

    criação do component institucional

---
+++ resources/views/components/site/institucional.blade.php
@@ -0,0 +1,23 @@
+<div class="row mt-3 text-justify">
+    <div class="col-lg-7 m-auto">
+        <h1 id="aboutUs">Institucional</h1>
+        <p class="lh-base fs-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, dicta
+            impedit velit officia veritatis asperiores nobis doloremque vel, corporis,
+            quia porro sequi consequatur laudantium dolore cumque voluptatibus sapiente
+            eligendi aliquid? Atque corrupti illo blanditiis eveniet eos, possimus fuga
+            sint iure! Tempore sequi enim consequatur sunt distinctio, explicabo ex vero
+            maxime ab dolorem debitis veritatis reiciendis culpa dignissimos! Quo, dolorum
+            commodi. Voluptates, cumque! Architecto, vel. Aliquam neque tempore nisi corrupti
+            facilis tenetur facere ipsam fugit ab, quod, enim aliquid doloremque odit molestiae
+            adipisci dolorem in? Saepe ut facilis fugiat at temporibus!
+        </p>
+        <div class="text-center">
+            <a class="btn btn-primary fs-3 mb-2" href="/about" role="button">Saiba mais</a>
+        </div>
+    </div>
+    <div class="col-lg-5">
+        <img src="{{ asset('storage/imagem/banner/banner-4.png') }}" class="card-img" width="500"
+            height="500" alt="">
+    </div>
+</div>
