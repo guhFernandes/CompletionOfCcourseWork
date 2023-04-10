a313e0b1e632e5869476405886e2c8499f416829
Gustavo Fernandes <gfernandesilva2904@gmail.com>
2023-04-09 22:09:26 -0300

    criação do component dicas-moda

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -219,29 +219,7 @@
 
     <!-- Inicio do Dica de Moda -->
     <div class="container">
-        <div class="row mt-5 text-justify">
-            <div class="col-lg-5">
-                <img src="{{ asset('storage/imagem/banner/banner-3.png') }}" class="card-img" width="500"
-                    height="500" alt="">
-            </div>
-            <div class="col-lg-7  m-auto">
-                <h1 id="dicademoda">Dica de Modas</h1>
-                <p class="lh-base fs-5">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, dicta
-                    impedit velit officia veritatis asperiores nobis doloremque vel, corporis,
-                    quia porro sequi consequatur laudantium dolore cumque voluptatibus sapiente
-                    eligendi aliquid? Atque corrupti illo blanditiis eveniet eos, possimus fuga
-                    sint iure! Tempore sequi enim consequatur sunt distinctio, explicabo ex vero
-                    maxime ab dolorem debitis veritatis reiciendis culpa dignissimos! Quo, dolorum
-                    commodi. Voluptates, cumque! Architecto, vel. Aliquam neque tempore nisi corrupti
-                    facilis tenetur facere ipsam fugit ab, quod, enim aliquid doloremque odit molestiae
-                    adipisci dolorem in? Saepe ut facilis fugiat at temporibus!
-                </p>
-                <div class="text-center">
-                    <a class="btn btn-primary fs-3 mb-2" href="/dicas" role="button">Saiba mais</a>
-                </div>
-            </div>
-        </div>
+        <x-site.dicas-moda/>
         <x-site.institucional/>
     </div>
     <!-- Fim do Dica de Moda -->
