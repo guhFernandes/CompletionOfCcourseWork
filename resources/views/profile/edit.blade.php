2ee970a6cbcbbe063d6dad2447b3c4a220952f38
Gustavo Fernandes <gfernandesilva2904@gmail.com>
2023-04-13 20:54:44 -0300

    Correção do bug de alterar senha

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,45 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.layout')
+@section('title', 'Editar Usuario')
+@section('content')
+    <x-dashboard.navbar/>
+    <div class="container w-75">
+        <div class="row mt-5">
+            <div class="col-lg-12 col-md-12 col-sm-12">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+                <form method="post" action="{{ route('profile.update') }}" class="shadow p-3 mb-4">
+                    <h3>Informação do Perfil</h3>
+                    <p>Atualize as informações de perfil e o endereço de e-mail da sua conta.</p>
+                    @csrf
+                    @method('patch')
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+                    <label for="name" class="form-label">Nome do Usuario</label>
+                    <x-text-input id="name" name="name" type="text" class="form-control form-control-lg" :value="old('name',$user->name )" required autofocus autocomplete="name" />
+                    
+                    <label for="email" class="form-label">Email</label>
+                    <x-text-input id="email" name="email" type="email" class="form-control form-control-lg" :value="old('email',$user->email  )" required autocomplete="username" />
+                    
+                    <button type="submit" class="btn btn-warning text-white mt-3">Editar</button>
+                </form>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
+                <form method="post" action="{{ route('password.update') }}"  class="shadow p-3">
+                    @csrf
+                    @method('put')
+                    
+                    <h3>Atualizar senha</h3>
+                    <p>Certifique-se de que sua conta esteja usando uma senha longa e aleatória para se manter segura.</p>
+
+                    <label for="current_password" class="form-label">Senha Atual</label>
+                    <x-text-input id="current_password" name="current_password" type="password" class="form-control form-control-lg" autocomplete="current-password" />
+
+                    <label for="current_password" class="form-label">Nova Senha</label>
+                    <x-text-input id="password" name="password" type="password" class="form-control form-control-lg" autocomplete="new-password" />
+
+                    <label for="current_password" class="form-label">Confirmar Senha</label>
+                    <x-text-input id="password_confirmation" name="password_confirmation" type="password" class="form-control form-control-lg" autocomplete="new-password" />
+
+                    <button type="submit" class="btn btn-warning text-white mt-3">Editar</button>
+                </form>
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
